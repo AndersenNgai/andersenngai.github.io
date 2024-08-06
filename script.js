f08db574d47ee30cb5438ae3c2350ebf200f2603
Andersen <97132038+AndersenNgai@users.noreply.github.com>
2024-08-06 17:53:40 +0000

    The lorem ipsum works now

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let longLorem = document.getElementById("fulltext");
 let button = document.querySelector("button");
 function loremButton(){
-    if longLorem.style.display == "hidden"{
+    let longLorem = document.getElementById("fulltext");
+    if (longLorem.style.display == "none" || longLorem.style.display == ""){
         longLorem.style.display = "block";
+        button.innerText = "Press to hide the full text";
     }
     else{
-        longLorem.style.display = "hidden"
+        longLorem.style.display = "none";
+        button.innerText = "Press to show the full text";
     }
 }
 button.addEventListener("click", loremButton);
