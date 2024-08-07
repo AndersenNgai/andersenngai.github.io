c375e9d8709c80829e1693bda3324b5a2be1f065
Andersen <97132038+AndersenNgai@users.noreply.github.com>
2024-08-07 16:00:59 +0000

    Dark Mode button

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let button = document.querySelector("button");
-function showLorem(lor){lor.style.display = "block";}
-function hideLorem(lor){lor.style.display = "none";}
-function loremButton(){
-    let longLorem = document.querySelectorAll(".longlorem");
-    if (button.innerText == "Press to show the full text"){
-        button.innerText = "Press to hide the full text";
-        longLorem.forEach(showLorem);
-    }
-    else{
-        button.innerText = "Press to show the full text";
-        longLorem.forEach(hideLorem);
-    }
+function activateDarkMode(){
+    let navbar = document.querySelector("nav");
+    navbar.style.backgroundColor = "#568BA4";
+    navbar.style.color = "#ffffff";
+    let navbarLinks = document.querySelectorAll("#navbar a");
+    navbarLinks.forEach(navbarLinksWhite);
+    document.querySelector("body").style.backgroundColor = "#000000";
+    articles = document.querySelectorAll(".article");
+    articles.forEach(articlesLightBlue);
 }
-button.addEventListener("click", loremButton);
+function navbarLinksWhite(link){link.style.color = "#ffffff"};
+function articlesLightBlue(ar){ar.style.backgroundColor = "#568BA4"};
+let darkModeButton = document.getElementById("darkmode");
+darkModeButton.addEventListener("click", activateDarkMode);
